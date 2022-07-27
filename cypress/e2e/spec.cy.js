5e43e884f1649fe3a4c4508768b6c37ee1ed5c02
thienhuynh98 <thien.huynh@bellevuecollege.edu>
2022-07-26 17:38:05 -0700

    Edit github action test

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,36 @@
+describe('Implicit Assertion', () => {
+  it('testing implicit assertion', () => {
+    cy.visit('http://socialscope.link/')
+    cy.contains("Start Here").click();
+    cy.get('input[name=query]').type('test')
+    cy.get('input[name=sentiment][value=false]').check()
+    cy.get('input[name=sentiment][value=true]')
+        .should('not.be.checked')
+    cy.get('input[name=sentiment][value=false]').check()
+    cy.get('input[name=sentiment][value=true]').check()
+    cy.get('input[name=sentiment][value=false]')
+        .should('not.be.checked')
+    cy.get('input[name=twitter]').uncheck()
+    cy.get('input[name=reddit]').uncheck()
+    cy.get('input[name=youtube]').uncheck()
+    cy.get('input[name=twitter]').check()
+    cy.get('input[name=reddit]').check()
+    cy.get('input[name=youtube]').check()
+    cy.get('input[name=twitter]')
+        .should('be.visible')
+        .and('be.checked')
+    cy.get('input[name=reddit]')
+        .should('be.visible')
+        .and('be.checked')
+    cy.get('input[name=youtube]')
+        .should('be.visible')
+        .and('be.checked')
+    cy.contains("Launch Search").click()
+  })
+})
+describe('Implicit Assertion', () => {
+  it('testing implicit assertion', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains("THIEN HUYNH DOING TESTING");
+  })
+})
